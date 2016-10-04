114fc7cce1fad931372f1dcf7666b59a1507ab4b
lucky500 <luciano101@msn.com>
2016-10-04 19:29:32 -0400

    fixed js logic and issues with modal

--- js/app1.js
+++ js/app1.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-var resultBtn = document.getElementById('finalResult');
+    console.log( "ready!" );
 
-  var preApprovalCheck = function(obj){
-  	if(obj.credit > 700 && obj.income > 100000  && obj.loan <= 800000) {
+    function showDiv() {
+    	document.getElementById('thisIsIt').style.display = "block";
+    };
+
+  /* var preApprovalCheck = function(obj){
+  	if(obj.credit > 700 && obj.income > 50000  && obj.loan < 200000) {
   		 return [true, 'Congratulations ' + obj.first + ' ' + obj.last + '! You are pre-qualified to receive a home loan of $' + obj.loan + '!'];
   	} else {
   		return [false, "We're sorry, " + obj.first + "; but, based on the information you provided, we aren't able to qualify you for this loan."];
@@ -10,6 +14,7 @@ var resultBtn = document.getElementById('finalResult');
   };
 
   document.querySelector('.js-run').addEventListener('click', function(){
+  	
   	// Create a variable obj and set the properties with values
   	var applicantInfo = {
   		first: document.querySelector('.js-first-name').value,
@@ -21,8 +26,7 @@ var resultBtn = document.getElementById('finalResult');
   	//Pass the object to the "preApprovalCheck" function
   	var result = preApprovalCheck(applicantInfo);
   	//Grab the output element to reference below
-  	var output = document.querySelector('output');
-    $('#final-result').append(output);
+  	var output = document.querySelector('output'); 
 
   	//If function returned the boolean "true" then add the class "approved" so it gets a nice
   	//look, otherwise, remove the class and give it a "not-approved"class
@@ -33,8 +37,34 @@ var resultBtn = document.getElementById('finalResult');
   		output.classList.remove('approved');
   		output.classList.add('not-approved');
   	}
-  	  output.textContent = result[1];
-  });
+  	output.textContent = result[1];
+  });*/
 
+/*==== Modal with AJAX request */
+ 	
 
-});
+ /*	BootstrapDialog.show({
+		        message: 'I send ajax request!',
+		        buttons: [{
+		            icon: 'glyphicon glyphicon-send',
+		            label: 'Send ajax request',
+		            cssClass: 'uniqueBtn',
+		            autospin: true,
+		            action: function(dialogRef){
+		                dialogRef.enableButtons(false);
+		                dialogRef.setClosable(false);
+		                dialogRef.getModalBody().html('Dialog closes in 5 seconds.');
+		                setTimeout(function(){
+		                    dialogRef.close();
+		                }, 5000);
+		            }
+		        }, {
+		            label: 'Close',
+		            action: function(dialogRef){
+		                dialogRef.close();
+		            }
+		        }]
+		    });
+
+*/
+		});
